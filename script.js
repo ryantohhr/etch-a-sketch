db1fab58c9099def943484409f66b0fc402aede1
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-26 22:27:55 +0800

    Implemented generateGrid

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-// Create grid
 const container = document.querySelector(".container");
 
-let n = 16
+let n = 16;
+generateGrid(n);
 
-for (let i = 0; i < n ** 2; i++) {
-    const grid = document.createElement("div");
-    const gridLength = 500 / n;
-    grid.style.cssText = `background-color: white; flex: 1 1 auto; height: ${gridLength}px; width: ${gridLength}px;`;
-    container.appendChild(grid);
+function generateGrid(n) {
+    for (let i = 0; i < n ** 2; i++) {
+        const grid = document.createElement("div");
+        const gridLength = 500 / n;
+        grid.style.cssText = `background-color: white; flex: 1 1 auto; height: ${gridLength}px; width: ${gridLength}px;`;
+        
+        // Create grid
+        container.appendChild(grid);
+    
+        // Enable drawing
+        grid.addEventListener('mouseenter', () => {
+            grid.style.cssText += "background-color: black;";
+        });
+    };
 }
-
-
-// Enable drawing
 
 
 
