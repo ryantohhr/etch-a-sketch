54960e3400da841e30b680efbdc2f8a61c48578e
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-26 15:51:08 +0800

    Added JS to generate grid

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// Create grid
 const container = document.querySelector(".container");
 
 let n = 16
 
 for (let i = 0; i < n ** 2; i++) {
     const grid = document.createElement("div");
+    const gridLength = 500 / n;
+    grid.style.cssText = `background-color: white; height: ${gridLength}px; width: ${gridLength}px;`;
     container.appendChild(grid);
 }
+
+
+// Enable drawing
+
+
+
+// Colour
